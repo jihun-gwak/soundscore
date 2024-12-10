60c7c50e2063cebfcb2a0ba903720aac6c28c6fb
Jihun Gwak <gjihun1395@gmail.com>
2024-12-09 23:59:55 -0700

    Update auth.js

--- src/app/_utils/auth.js
+++ src/app/_utils/auth.js
@@ -75,9 +75,5 @@ export const AuthContextProvider = ({ children }) => {
 };
 
 export const useUserAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error("useUserAuth must be used within an AuthContextProvider");
-  }
-  return context;
+  return useContext(AuthContext);
 };
